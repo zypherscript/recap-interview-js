f9943d8b7741f9d82c8551b90bbcc8c70c8c1179
zypherscript <kitti.kthp@gmail.com>
2023-06-03 02:53:43 +0700

    idek

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import { arrInfoList } from './arrInfo.js';
 function App() {
   return (
     <>
+      <h2 class="text-2xl font-medium text-gray-900 title-font mb-2 text-left">Array 101</h2>
       <ArrInfo />
-      <Form />
+      <h2 class="text-2xl font-medium text-gray-900 title-font mb-2 text-left">Promise(reject, resolve) and Async/Await</h2>
+      <QuizForm />
     </>
   )
 }
@@ -72,13 +74,23 @@ function ArrInfo() {
   );
 }
 
-function Form() {
+function QuizForm() {
   const [answer, setAnswer] = useState('');
   const [error, setError] = useState(null);
   const [status, setStatus] = useState('typing');
 
   if (status === 'success') {
-    return <h1>That's right! (Even if we apply to be a Java Developer, we also need to know about the frontend stuff tooooooooooooooooooo)</h1>
+    return (
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-col px-5 py-12 justify-center items-center">
+          <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+            <h3 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 title-font">
+              That's right! (Even if we apply to be a Java Developer, we also need to know about the frontend stuff tooooooooooooooooooo)
+            </h3>
+          </div>
+        </div>
+      </section>
+    );
   }
 
   async function handleSubmit(e) {
@@ -93,7 +105,7 @@ function Form() {
     }
   }
 
-  function handleTextareaChange(e) {
+  function handleInputChange(e) {
     setAnswer(e.target.value);
   }
 
@@ -112,31 +124,42 @@ function Form() {
   }
 
   return (
-    <>
-      <h2>Easy quiz</h2>
-      <p>
-        What is the frontend framework we are learning now?
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === 'submitting'}
-        />
-        <br />
-        <button disabled={
-          answer.length === 0 ||
-          status === 'submitting'
-        }>
-          Submit
-        </button>
-        {error !== null &&
-          <p className="Error">
-            {error.message}
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-col px-5 py-12 justify-center items-center">
+        <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+          <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 title-font">
+            Easy quiz
+          </h2>
+          <p className="mb-8 leading-relaxed">
+            What is the frontend framework we are learning now?
           </p>
-        }
-      </form>
-    </>
+          <div className="flex w-full justify-center items-end">
+            <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
+              <input
+                type="text"
+                value={answer}
+                onChange={handleInputChange}
+                disabled={status === 'submitting'}
+                className={`w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-indigo-200 focus:bg-transparent border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out ${status === 'submitting' ? 'opacity-50 cursor-not-allowed' : ''}`}
+              />
+            </div>
+            <button className={`inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg ${answer.length === 0 || status === 'submitting' ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={
+                answer.length === 0 ||
+                status === 'submitting'
+              }
+              onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+          {error !== null &&
+            <p className="mt-4 leading-relaxed text-orange-900">
+              {error.message}
+            </p>
+          }
+        </div>
+      </div>
+    </section>
   );
 }
 
